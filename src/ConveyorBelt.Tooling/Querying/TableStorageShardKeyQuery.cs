f9f80e495b78cc177c92f22ebd5410fb70e876e4
Sergey Zhurbenko <sergey.zhurbenko@solomoto.com>
2016-02-19 14:51:39 +0300

    fix range selector in Querying

--- src/ConveyorBelt.Tooling/Querying/TableStorageShardKeyQuery.cs
+++ src/ConveyorBelt.Tooling/Querying/TableStorageShardKeyQuery.cs
@@ -16,9 +16,17 @@ namespace ConveyorBelt.Tooling.Querying
             var account = CloudStorageAccount.Parse(shardKeyArrived.Source.ConnectionString);
             var client = account.CreateCloudTableClient();
             var table = client.GetTableReference(shardKeyArrived.Source.DynamicProperties["TableName"].ToString());
-            var Filter = TableQuery.GenerateFilterCondition("PartitionKey", "eq", new DateTime(long.Parse(shardKeyArrived.ShardKey)).ToString("yyyyMMdd-hhmms"));
+            var shardKeyEnd = long.Parse(shardKeyArrived.ShardKey) + 10000000;
+            var Filter = new TableQuery().Where(
+                TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey", "ge", new DateTime(long.Parse(shardKeyArrived.ShardKey)).ToString("yyyyMMdd-hhmms")),
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("PartitionKey", "le", new DateTime(shardKeyEnd).ToString("yyyyMMdd-hhmms"))));
             return Task.FromResult(table.ExecuteQuery(new TableQuery().Where(
-                TableQuery.GenerateFilterCondition("PartitionKey", "eq", new DateTime(long.Parse(shardKeyArrived.ShardKey)).ToString("yyyyMMdd-hhmms")))));
+                TableQuery.CombineFilters(
+                TableQuery.GenerateFilterCondition("PartitionKey", "ge", new DateTime(long.Parse(shardKeyArrived.ShardKey)).ToString("yyyyMMdd-hhmms")), 
+                TableOperators.And,
+                TableQuery.GenerateFilterCondition("PartitionKey", "le", new DateTime(shardKeyEnd).ToString("yyyyMMdd-hhmms"))))));
         }
     }
 }
